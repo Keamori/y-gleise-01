7c7ba908f7f31821e644b7d2b3609d1db9f433a1
Keamori <rosacenaorion@gmail.com>
2025-04-28 14:31:49 +0800

    Add files via upload

---
+++ web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserLogin;
+use App\Http\Controllers\UserRegister;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', [UserLogin::class,'index'])->name('user.login');
+
+Route::get('/register', [UserRegister::class,'index'])->name('user.register');
+
+Route::post('/user/create', [UserRegister::class,'store'])->name('user.create');
+
+Route::post('/user/auth', [UserLogin::class,'login'])->name('user.auth');
